c0f9636606a8f4418cce9857c561257cb358f6ec
Alex_Portas <alexalonsoportas@gmail.com>
2019-02-27 23:16:10 +0100

    Video 78! No me crea los botones..

--- src/graficos/PruebaAcciones.java
+++ src/graficos/PruebaAcciones.java
@@ -4,6 +4,9 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.event.*;
 import javax.swing.*;
+import static javax.swing.Action.NAME;
+import static javax.swing.Action.SHORT_DESCRIPTION;
+import static javax.swing.Action.SMALL_ICON;
 
 public class PruebaAcciones {
     public static void main(String[]args){
@@ -18,15 +21,19 @@ class MarcoAccion extends JFrame{
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setBounds(600, 300, 500, 350);
         setTitle("Respondiendo");
-        add(new LaminaAccion());
+        LaminaAccion lamina = new LaminaAccion();
+        add(lamina);
    }
 }
 
 class LaminaAccion extends JPanel{
     public LaminaAccion(){
-        AccionColor accionAmarillo=new AccionColor("Amarillo", null, Color.yellow)
-        AccionColor accionAzul=new AccionColor("Azul", null, Color.yellow)
-        AccionColor accionRojo=new AccionColor("Rojo", null, Color.yellow)
+        AccionColor accionAmarillo=new AccionColor("Amarillo", null, Color.yellow);
+        AccionColor accionAzul=new AccionColor("Azul", null, Color.blue);
+        AccionColor accionRojo=new AccionColor("Rojo", null, Color.red);
+        add(new JButton(accionAmarillo));
+        add(new JButton(accionAzul));
+        add(new JButton(accionRojo));
         /*JButton botonAmarillo=new JButton("Amarillo");
         JButton botonAzul=new JButton("Azul");
         JButton botonRojo=new JButton("Rojo");
@@ -34,19 +41,19 @@ class LaminaAccion extends JPanel{
         add(botonAzul);
         add(botonRojo);*/
     }
+ 
+    private class AccionColor extends AbstractAction{
+
+        public void actionPerformed(ActionEvent e) {
+            Color c = (Color) getValue("color_de_fondo");
+            setBackground(c);
+        }
+
+        public AccionColor(String nombre, Icon icono, Color color) {
+            putValue(NAME, nombre);
+            putValue(SMALL_ICON, icono);
+            putValue(SHORT_DESCRIPTION, "Poner la lámina de color "+nombre);
+            putValue("color_de_fondo", color);
+        }
+    } 
 }
-
-class AccionColor extends AbstractAction{
-
-    public void actionPerformed(ActionEvent e) {
-        
-    }
-
-    public AccionColor(String nombre, Icon icono, Color color) {
-        putValue(NAME, nombre);
-        putValue(SMALL_ICON, icono);
-        putValue(SHORT_DESCRIPTION, "Poner la lámina de color "+nombre);
-        putValue("color_de_fondo", color);
-    }
-    
-} 

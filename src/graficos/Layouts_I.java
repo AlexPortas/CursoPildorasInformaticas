da0e09291c9e39d3fd5a8cb65f6f397a4e0564a2
Alex_Portas <alexalonsoportas@gmail.com>
2019-03-20 22:31:54 +0100

    Video 82

--- src/graficos/Layouts_I.java
+++ src/graficos/Layouts_I.java
@@ -19,15 +19,29 @@ public class Layouts_I {
             setVisible(true);
             setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             Panel_Layout lamina= new Panel_Layout();
-            FlowLayout disposicion=new FlowLayout(FlowLayout.LEFT);
-            lamina.setLayout(disposicion);
-            add(lamina);
+            /*FlowLayout disposicion=new FlowLayout(FlowLayout.LEFT);
+            lamina.setLayout(disposicion);*/
+            add(lamina, BorderLayout.NORTH);
+            Panel_Layout2 lamina2= new Panel_Layout2();
+            add(lamina2, BorderLayout.SOUTH);
         }
     }
 
     private static class Panel_Layout extends JPanel{
 
         public Panel_Layout() {
+            //setLayout(new FlowLayout(FlowLayout.LEFT));
+            setLayout(new BorderLayout());
+            add(new JButton("Amarillo"),BorderLayout.EAST);
+            add(new JButton("Rojo"),BorderLayout.SOUTH);
+            add(new JButton("Azul"),BorderLayout.WEST);
+        }
+    }
+    
+    private static class Panel_Layout2 extends JPanel{
+
+        public Panel_Layout2() {
+            setLayout(new FlowLayout(FlowLayout.LEFT));
             add(new JButton("Amarillo"));
             add(new JButton("Rojo"));
             add(new JButton("Azul"));

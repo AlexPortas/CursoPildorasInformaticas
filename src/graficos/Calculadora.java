5aebb254273202ca1aa9a585275d34c36d9e5508
Alex_Portas <alexalonsoportas@gmail.com>
2019-03-30 23:49:18 +0100

    videos 87 e 86

--- src/graficos/Calculadora.java
+++ src/graficos/Calculadora.java
@@ -2,6 +2,8 @@
 package graficos;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -15,75 +17,92 @@ public class Calculadora {
 class MarcoCalculadora extends JFrame{
     public MarcoCalculadora() {
         setTitle("Calculadora");
-        setBounds(600, 350, 600, 300);
+        setBounds(500, 300, 450, 300);
         setVisible(true);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         LaminaCalculadora lamina= new LaminaCalculadora();
         add(lamina, BorderLayout.NORTH);
+        //pack();
     }
 }
 
 
 class LaminaCalculadora extends JPanel{
     private JPanel lamina2;
+    private JButton pantalla;
+    private boolean principio;
+    private double resultado;
+    private String ultimaOrden;
     
     public LaminaCalculadora() {
+        principio=true;
         setLayout(new BorderLayout());
-        JButton pantalla=new JButton("0");
+        pantalla=new JButton("0");
         pantalla.setEnabled(false);
         add(pantalla,BorderLayout.NORTH);
         lamina2 =new JPanel();
+        ActionListener inserta = new InsertaNumero();
+        ActionListener orden = new AccionOrden();
         lamina2.setLayout(new GridLayout(4,4));
-        /*JButton btn1 = new JButton("1");
-        lamina2.add(btn1);
-        JButton btn2 = new JButton("2");
-        lamina2.add(btn2);
-        JButton btn3 = new JButton("3");
-        lamina2.add(btn3);
-        JButton btn4 = new JButton("4");
-        lamina2.add(btn4);
-        JButton btn5 = new JButton("5");
-        lamina2.add(btn5);
-        JButton btn6 = new JButton("6");
-        lamina2.add(btn6);
-        JButton btn7 = new JButton("7");
-        lamina2.add(btn7);
-        JButton btn8 = new JButton("8");
-        lamina2.add(btn8);
-        JButton btn9 = new JButton("9");
-        lamina2.add(btn9);
-        JButton btn0 = new JButton("0");
-        lamina2.add(btn0);
-        JButton btnig = new JButton("=");
-        lamina2.add(btnig);
-        JButton btndiv = new JButton("/");
-        lamina2.add(btndiv);
-        JButton btnsum = new JButton("+");
-        lamina2.add(btnsum);
-        JButton btnres = new JButton("-");
-        lamina2.add(btnres);
-        JButton btnmul = new JButton("*");
-        lamina2.add(btnmul);*/
-        ponerBoton("7");
-        ponerBoton("8");
-        ponerBoton("9");
-        ponerBoton("/");
-        ponerBoton("4");
-        ponerBoton("5");
-        ponerBoton("6");
-        ponerBoton("*");
-        ponerBoton("1");
-        ponerBoton("2");
-        ponerBoton("3");
-        ponerBoton("*");
-        ponerBoton("0");
-        ponerBoton(".");
-        ponerBoton("=");
-        ponerBoton("+");
+        ponerBoton("7", inserta);
+        ponerBoton("8", inserta);
+        ponerBoton("9", inserta);
+        ponerBoton("/", orden);
+        ponerBoton("4", inserta);
+        ponerBoton("5", inserta);
+        ponerBoton("6", inserta);
+        ponerBoton("*", orden);
+        ponerBoton("1", inserta);
+        ponerBoton("2", inserta);
+        ponerBoton("3", inserta);
+        ponerBoton("*", orden);
+        ponerBoton("0", inserta);
+        ponerBoton(".", inserta);
+        ponerBoton("=", orden);
+        ponerBoton("+", orden);
         add(lamina2, BorderLayout.CENTER);
     }
-    private void ponerBoton(String rotulo){
+    
+    private void ponerBoton(String rotulo, ActionListener oyente){
         JButton boton = new JButton(rotulo);
+        boton.addActionListener(oyente);
         lamina2.add(boton);
     }
+    
+    private class InsertaNumero implements ActionListener{
+        public void actionPerformed(ActionEvent e) {
+            String entrada = e.getActionCommand();
+            if (principio){
+                pantalla.setText(entrada);   
+                principio = false;
+            } else {
+                pantalla.setText(pantalla.getText() + entrada);
+            }
+        }     
+    }
+    
+    private class AccionOrden implements ActionListener{
+        public void actionPerformed(ActionEvent e) {
+            principio = true;
+            String simbolo = e.getActionCommand();
+            calcular(Double.parseDouble(pantalla.getText()));
+            ultimaOrden = simbolo;
+        }
+        
+        public void calcular(double x){
+            if(ultimaOrden.equals("+")){
+                resultado+=x;
+            } else if(ultimaOrden.equals("-")){
+                resultado-=x;
+            } else if(ultimaOrden.equals("*")){
+                resultado*=x;
+            } else if(ultimaOrden.equals("/")){
+                resultado/=x;
+            } else{
+                resultado=x;
+            }
+            pantalla.setText("" + resultado);
+        }
+    }
 }
+
---
+++ src/graficos/PruebaTexto.java
@@ -0,0 +1,67 @@
+
+package graficos;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
+
+public class PruebaTexto {
+    public static void main(String[]args){
+        MarcoTexto marco= new MarcoTexto();
+    }
+}
+
+class MarcoTexto extends JFrame{
+    public MarcoTexto() {
+        setTitle("Prueba texto");
+        setBounds(500, 300, 450, 300);
+        setVisible(true);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        LaminaTexto lamina= new LaminaTexto();
+        add(lamina);
+    }
+}
+
+
+class LaminaTexto extends JPanel{
+    private JTextField campo1;
+    private JLabel resultado;
+    
+    public LaminaTexto() {
+        setLayout(new BorderLayout());
+        JPanel lamina2 = new JPanel();
+        lamina2.setLayout(new FlowLayout());
+        resultado = new JLabel("", JLabel.CENTER);
+        JLabel texto1 = new JLabel("Email:");
+        lamina2.add(texto1);
+        campo1 = new JTextField(20);
+        lamina2.add(campo1);
+        add(resultado, BorderLayout.CENTER);
+        JButton btn = new JButton("Comprobar");
+        DameTexto mievento = new DameTexto();
+        btn.addActionListener(mievento);
+        lamina2.add(btn);
+        add(lamina2, BorderLayout.NORTH);
+    }
+
+    private class DameTexto implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            int correcto = 0;
+            String email = campo1.getText();
+            for (int i = 0; i < email.length(); i++) {
+                if(email.charAt(i)=='@'){
+                    correcto++;
+                }
+            }
+            if(correcto!=1){
+                resultado.setText("Email incorrecto");
+            } else {
+                resultado.setText("Email correcto");
+            }
+        }
+    }
+}
+

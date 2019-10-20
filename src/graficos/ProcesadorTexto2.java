09dce30175a7e03dd46fe837fad85f94234127a6
Alex_Portas <alexalonsoportas@gmail.com>
2019-10-20 20:11:27 +0200

    video 106

--- src/graficos/ProcesadorTexto2.java
+++ src/graficos/ProcesadorTexto2.java
@@ -2,34 +2,33 @@ package graficos;
 
 import java.awt.BorderLayout;
 import java.awt.Font;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import javax.swing.*;
+import javax.swing.text.StyledEditorKit;
 
-public class ProcesadorTexto {
+public class ProcesadorTexto2 {
     public static void main(String[]args){
-        MarcoProText marco= new MarcoProText();
+        MarcoProText2 marco= new MarcoProText2();
     }
 }
 
-class MarcoProText extends JFrame{
-    public MarcoProText() {
+class MarcoProText2 extends JFrame{
+    public MarcoProText2() {
         setTitle("Procesaddor de texto");
         setBounds(500, 300, 450, 300);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         
-        add(new LaminaProText());
+        add(new LaminaProText2());
         
         setVisible(true);
     }
 }
 
-class LaminaProText extends JPanel {
+class LaminaProText2 extends JPanel {
     private JTextPane textarea;
     JMenu fuente, estilo, tamanho;
     private Font letras;
     
-    public LaminaProText() {
+    public LaminaProText2() {
         setLayout(new BorderLayout());
         JMenuBar barra = new JMenuBar();
         fuente = new JMenu("Fuente");
@@ -57,45 +56,17 @@ class LaminaProText extends JPanel {
         JMenuItem elem_menu = new JMenuItem(rotulo);
         if (menu=="fuente"){
             fuente.add(elem_menu);
+            elem_menu.addActionListener(new StyledEditorKit.FontFamilyAction("cambia tipo letra", letra));
         }else if (menu=="estilo"){
             estilo.add(elem_menu);
+            if (par_estilo==1){
+                elem_menu.addActionListener(new StyledEditorKit.BoldAction());
+            } else if (par_estilo==2){
+                elem_menu.addActionListener(new StyledEditorKit.ItalicAction());
+            }
         }else{
             tamanho.add(elem_menu);
-        }
-        elem_menu.addActionListener(new GestionaEventos(rotulo, letra, par_estilo, par_tamanho));
-    }
-     
-    private class GestionaEventos implements ActionListener{
-        String fuente_texto, menu;
-        int estilo_texto, tamanho_texto;
-
-        public GestionaEventos(String menu, String fuente_texto, int estilo_texto, int tamanho_texto) {
-            this.fuente_texto = fuente_texto;
-            this.menu = menu;
-            this.estilo_texto = estilo_texto;
-            this.tamanho_texto = tamanho_texto;
-        }
-
-
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            letras = textarea.getFont();
-            if(menu=="Arial" || menu== "Verdana" || menu=="Courier"){
-                estilo_texto = letras.getStyle();
-                tamanho_texto = letras.getSize();
-            }
-            if(menu=="Cursiva" || menu== "Negrita"){
-                if (letras.getStyle()==1 || letras.getStyle()==2){
-                    estilo_texto=3;
-                }
-                fuente_texto = letras.getFontName();
-                tamanho_texto = letras.getSize();
-            }
-            if(menu=="12" || menu== "24" || menu=="36"){
-                fuente_texto = letras.getFontName();
-                estilo_texto = letras.getStyle();
-            }
-            textarea.setFont(new Font(fuente_texto, estilo_texto, tamanho_texto));
+            elem_menu.addActionListener(new StyledEditorKit.FontSizeAction("cambia tamaño", par_tamanho));
         }
     }
 }
